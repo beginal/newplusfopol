6f1e23ed85654584f29fe26e88d4c6dc01674e31
beginal <beginal01@gmail.com>
2020-06-23 11:19:39 +0900

    Updates

---
+++ precache-manifest.5b8bbf399421899e738b0aa15e283b63.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccd49f21c4bfc448092f38a0cfd4b3d0",
+    "url": "/newpluspofol/index.html"
+  },
+  {
+    "revision": "ca882f8745e9a1a338b1",
+    "url": "/newpluspofol/static/js/2.3daba3e1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/newpluspofol/static/js/2.3daba3e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a5bec1929d3319fa8e7",
+    "url": "/newpluspofol/static/js/main.786883d6.chunk.js"
+  },
+  {
+    "revision": "0b083e0883fef351f426",
+    "url": "/newpluspofol/static/js/runtime-main.05d471f5.js"
+  }
+]);
